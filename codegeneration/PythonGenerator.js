ecc6d0ca49d31f5bf3a41ae0558610b6e81acf4a
Shakyani Jayasiriwardene <shakyani.jayasiriwardene@ifsworld.com>
2018-10-31 14:04:59 +0530

    my commit 2

--- codegeneration/PythonGenerator.js
+++ codegeneration/PythonGenerator.js
@@ -53,6 +53,95 @@ visitPropertyExpressionAssignment(ctx) {
   const value = this.visit(ctx.singleExpression());
 
   return `'${key}': ${value}`;
+
+  //Return the text of all tokens in the stream
+  console.log(ctx.gettext());
+
+  //How many children are there? If there is none, then this node represents a leaf node
+  console.log(ctx.getChildCount());
+
+  //console.log(ctx.propertyName().getText()) 'x' property
+  console.log(ctx.getChild(0).getText());
+
+  //:
+  console.log(ctx.getChild(1).getText());
+
+  //console.log(ctx.singleExpression().getText()) '1' value
+  console.log(ctx.getChild(2).getText());
+}
+
+/**
+ * Because Python doesn't need `New`, we can skip the first child
+ *
+ * @param {object} ctx
+ * @returns {string}
+ */
+visitNewExpression(ctx) {
+  return this.visit(ctx.singleExpression());
+}
+
+/**
+ * Visits `Number` Keyword
+ *
+ * @param {object} ctx
+ * @returns {string}
+ */
+visitNumberExpression(ctx) {
+  const argumentList = ctx.arguments().argumentList();
+
+  // JavaScript Number requires only one argument,
+  // otherwise method returns error message 
+  if (argumentList === null || argumentList.getChildCount() !== 1) {
+    return 'Error: Number requires one argument';
+  }
+
+  const arg = argumentList.singleExpression()[0];
+  const number = this.removeQuotes(this.visit(arg));
+
+  return `int(${number})`;
+}
+
+/**
+ * Removes quotes from string
+ *
+ * @param {String} str
+ * @returns {String}
+ */
+removeQuotes(str) {
+  let newStr = str;
+
+  if (
+    (str.charAt(0) === '"' && str.charAt(str.length - 1) === '"') ||
+    (str.charAt(0) === '\'' && str.charAt(str.length - 1) === '\'')
+  ) {
+    newStr = str.substr(1, str.length - 2);
+  }
+
+  return newStr;
+}
+
+const path = require('path');
+const {
+  SemanticArgumentCountMismatchError
+} = require(path.resolve('error', 'helper'));
+
+/**
+ * Visits `Number` Keyword
+ *
+ * @param {object} ctx
+ * @returns {string}
+ */
+visitNumberExpression(ctx) {
+  const argumentList = ctx.arguments().argumentList();
+
+  if (argumentList === null || argumentList.getChildCount() !== 1) {
+    throw new SemanticArgumentCountMismatchError();
+  }
+
+  const arg = argumentList.singleExpression()[0];
+  const number = this.removeQuotes(this.visit(arg));
+
+  return `int(${number})`;
 }
 
 }
--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const antlr4 = require('antlr4');
 const ECMAScriptLexer = require('./lib/ECMAScriptLexer.js');
 const ECMAScriptParser = require('./lib/ECMAScriptParser.js');
 const PythonGenerator = require('./codegeneration/PythonGenerator.js');
+const ErrorListener = require('./codegeneration/ErrorListener.js');
 
-const input = '{x: 1}';
+const input = '{x: new Number(1)}';
 
 const chars = new antlr4.InputStream(input);
 const lexer = new ECMAScriptLexer.ECMAScriptLexer(chars);
@@ -12,14 +13,23 @@ lexer.strictMode = false; // do not use js strictMode
 
 const tokens = new antlr4.CommonTokenStream(lexer);
 const parser = new ECMAScriptParser.ECMAScriptParser(tokens);
-const tree = parser.program();
+const listener = new ErrorListener();
 
-//console.log(tree.toStringTree(parser.ruleNames));
+// Do this after creating the parser and before running it
+parser.removeErrorListeners(); // Remove default ConsoleErrorListener
+parser.addErrorListener(listener); // Add custom error listener
 
 console.log('JavaScript input:');
 console.log(input);
 console.log('Python output:');
 
-const output = new PythonGenerator().start(tree);
+try {
+  const tree = parser.expressionSequence();
+  const output = new PythonGenerator().start(tree);
 
-console.log(output);
+  console.log(output);
+
+  // console.log(tree.toStringTree(parser.ruleNames));
+} catch (error) {
+  console.log(error);
+}

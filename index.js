ff7b5c01718b669b1b64b33e3c2698fc755784ba
Shakyani Jayasiriwardene <shakyani.jayasiriwardene@ifsworld.com>
2018-10-31 11:29:14 +0530

    my commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const antlr4 = require('antlr4');
+const ECMAScriptLexer = require('./lib/ECMAScriptLexer.js');
+const ECMAScriptParser = require('./lib/ECMAScriptParser.js');
+const PythonGenerator = require('./codegeneration/PythonGenerator.js');
+
+const input = '{x: 1}';
+
+const chars = new antlr4.InputStream(input);
+const lexer = new ECMAScriptLexer.ECMAScriptLexer(chars);
+
+lexer.strictMode = false; // do not use js strictMode
+
+const tokens = new antlr4.CommonTokenStream(lexer);
+const parser = new ECMAScriptParser.ECMAScriptParser(tokens);
+const tree = parser.program();
+
+//console.log(tree.toStringTree(parser.ruleNames));
+
+console.log('JavaScript input:');
+console.log(input);
+console.log('Python output:');
+
+const output = new PythonGenerator().start(tree);
+
+console.log(output);
